1ef301d0027995fc5f8afe6b1d09c008537f6871
Picheri Likitha <121708442+Likithasowji-500k@users.noreply.github.com>
2024-06-14 20:56:28 +0530

    Time: 59 ms (5.38%), Space: 16.3 MB (99.83%) - LeetHub

---
+++ 2886-faulty-keyboard/2886-faulty-keyboard.py
@@ -0,0 +1,12 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        t=''
+        for i in s:
+            if i=='i':
+                t=t[::-1]
+            else:
+                t=t+i
+        return t
+
+                
+        
