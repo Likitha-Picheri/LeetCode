af4998c2a6c8c6af81a76080a0065a06c66a5626
Picheri Likitha <121708442+Likithasowji-500k@users.noreply.github.com>
2024-06-18 06:52:49 +0530

    Time: 110 ms (38.93%), Space: 16.5 MB (39.36%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -4,8 +4,6 @@ class Solution:
         t=math.floor(math.sqrt(c))
         for i in range(t+1):
             if (math.sqrt(c-(i**2))).is_integer():
-                print(math.sqrt(c-i**2))
-                print(i)
                 return True
             
         return False
