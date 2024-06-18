ad6a12d0231dd0ee93bd4ec44388a166b8851bf1
Picheri Likitha <121708442+Likithasowji-500k@users.noreply.github.com>
2024-06-18 06:52:32 +0530

    Time: 97 ms (52.3%), Space: 16.6 MB (15.4%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -2,8 +2,6 @@ import math
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
         t=math.floor(math.sqrt(c))
-        if c==2 or c==1:
-            return True
         for i in range(t+1):
             if (math.sqrt(c-(i**2))).is_integer():
                 print(math.sqrt(c-i**2))
