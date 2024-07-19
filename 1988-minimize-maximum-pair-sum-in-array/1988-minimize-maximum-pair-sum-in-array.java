7ffb181db380890ce0f9f6811714a121f5b1262e
Picheri Likitha <121708442+Likitha-Picheri@users.noreply.github.com>
2024-07-19 21:40:17 +0530

    Time: 52 ms (90.53%), Space: 58.1 MB (67.52%) - LeetHub

--- 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.java
+++ 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.java
@@ -5,7 +5,7 @@ class Solution {
         int max=0;
         for(int i=0;i<t/2;i++){
            int sum=nums[i]+nums[t-i- 1];
-            if(max<nums[i]+nums[t-i- 1]){
+            if(max<sum){
                 max=sum;
             }
         }
